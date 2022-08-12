4f169d04887646a2bf892354b1ed5c8a2f663af2
OMGDuke <alex@alexrobertson.dev>
2022-08-12 08:13:31 +0100

    update packages and shorten afterPatch by a layer

--- src/index.tsx
+++ src/index.tsx
@@ -13,19 +13,15 @@ import ProtonMedal from './components/protonMedal';
 
 export default definePlugin((serverAPI: ServerAPI) => {
   const patch = serverAPI.routerHook.addPatch('/library/app/:appid', (props: {path: string; children: ReactElement}) => {
-    afterPatch(props.children.type, 'type', (_: any, ret: any) => {
-      afterPatch(ret.props.children[0].props, 'renderChildrenFunc', (_2: any, ret2: any) => {
-        afterPatch(ret2.props.children.type, 'type', (_3: any, ret3: any) => {
-          // Check if the component already exists before we try to add it
-          const alreadySpliced = Boolean(ret3.props.children[1].props.children.props.children.find(
-            (child: any) => child?.props?.className === 'protondb-decky-indicator',
-          ));
-          if (!alreadySpliced) {
-            ret3.props.children[1].props.children.props.children
-              .splice(1, 0, <ProtonMedal serverAPI={serverAPI} className="protondb-decky-indicator" />);
-          }
-          return ret3;
-        });
+    afterPatch(props.children.props, 'renderFunc', (_: any, ret: any) => {
+      afterPatch(ret.props.children.type, 'type', (_2: any, ret2: any) => {
+        const alreadySpliced = Boolean(ret2.props.children[1].props.children.props.children.find(
+          (child: any) => child?.props?.className === 'protondb-decky-indicator',
+        ));
+        if (!alreadySpliced) {
+          ret2.props.children[1].props.children.props.children
+            .splice(1, 0, <ProtonMedal serverAPI={serverAPI} className="protondb-decky-indicator" />);
+        }
         return ret2;
       });
       return ret;
