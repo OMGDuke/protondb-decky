560d95bfcbcfc239c3de84a1788a9f4d67662b06
OMGDuke <alex@alexrobertson.dev>
2022-08-17 19:55:15 +0100

    add size and position settings

--- src/components/protonMedal/index.tsx
+++ src/components/protonMedal/index.tsx
@@ -1,5 +1,5 @@
 import { Button, ButtonProps, Router, ServerAPI } from 'decky-frontend-lib'
-import { ReactElement, FC, CSSProperties } from 'react'
+import { ReactElement, FC, CSSProperties, ReactNode } from 'react'
 import { FaReact } from 'react-icons/fa'
 import { IoLogoTux } from 'react-icons/io'
 import { useSettings } from '../../context/settingsContext'
@@ -11,12 +11,20 @@ import useProtonDBTier from '../../hooks/useProtonDBTier'
 import './protonMedal.css'
 
 type ExtendedButtonProps = ButtonProps & {
-  children: ReactElement | ReactElement[]
+  children: ReactNode
+
   type: 'button'
   style: CSSProperties
 }
 
 const DeckButton = Button as FC<ExtendedButtonProps>
+
+const positonSettings = {
+  tl: { top: '40px', left: '20px' },
+  tr: { top: '60px', right: '20px' },
+  bl: { bottom: '40px', left: '20px' },
+  br: { bottom: '40px', right: '20px' }
+}
 
 export default function ProtonMedal({
   serverAPI,
@@ -32,6 +40,32 @@ export default function ProtonMedal({
   const { state } = useSettings()
 
   if (!protonDBTier) return <></>
+  if (state.size === 'minimalist') {
+    return (
+      <DeckButton
+        className={className}
+        type="button"
+        onClick={() => {
+          Router.NavigateToExternalWeb(`https://www.protondb.com/app/${appId}`)
+        }}
+        style={{
+          background: protonDBTier?.backgroundColor,
+          color: protonDBTier?.textColor,
+          padding: '6px',
+          ...positonSettings[state.position]
+        }}
+      >
+        {linuxSupport ? (
+          <IoLogoTux size={20} style={{ marginRight: 10 }} />
+        ) : (
+          <></>
+        )}
+        {/* The ProtonDB logo has a distracting background, so React's logo is being used as a close substitute */}
+        <FaReact size={20} />
+      </DeckButton>
+    )
+  }
+
   return (
     <DeckButton
       className={className}
@@ -41,37 +75,37 @@ export default function ProtonMedal({
       }}
       style={{
         background: protonDBTier?.backgroundColor,
-        color: protonDBTier?.textColor
+        color: protonDBTier?.textColor,
+        flexDirection: state.size === 'small' ? 'column' : 'row',
+        padding: state.size === 'small' ? '6px 8px ' : '6px 18px',
+        ...positonSettings[state.position]
       }}
     >
-      {linuxSupport ? (
-        <IoLogoTux size={28} color={protonDBTier?.textColor} />
-      ) : (
-        <></>
-      )}
-      {/* The ProtonDB logo has a distracting background, so React's logo is being used as a close substitute */}
-      <FaReact
-        size={28}
-        color={protonDBTier?.textColor}
-        style={{ marginLeft: linuxSupport ? 10 : 0 }}
-      />
-      {state.minimalist ? (
-        <></>
-      ) : (
-        <span
-          style={{
-            marginLeft: 10,
-            fontSize: 24,
-            width: '132px',
-            textTransform: 'uppercase',
-            fontFamily: 'Abel,"Motiva Sans",Arial,Helvetica,sans-serif',
-            lineHeight: '24px',
-            marginRight: '28px'
-          }}
-        >
-          {protonDBTier.label}
-        </span>
-      )}
+      <div>
+        {linuxSupport ? (
+          <IoLogoTux
+            size={state.size === 'small' ? 20 : 28}
+            style={{ marginRight: 10 }}
+          />
+        ) : (
+          <></>
+        )}
+        {/* The ProtonDB logo has a distracting background, so React's logo is being used as a close substitute */}
+        <FaReact size={state.size === 'small' ? 20 : 28} />
+      </div>
+      <span
+        style={{
+          marginLeft: state.size === 'small' ? 0 : '10px',
+          fontSize: state.size === 'small' ? '12px' : '24px',
+          width: state.size === 'small' ? 'auto' : '132px',
+          lineHeight: state.size === 'small' ? '12px' : '24px',
+          marginRight: state.size === 'small' ? 0 : '28px'
+        }}
+      >
+        {state.size === 'small'
+          ? protonDBTier.label.slice(0, 4)
+          : protonDBTier.label}
+      </span>
     </DeckButton>
   )
 }
--- src/context/settingsContext.tsx
+++ src/context/settingsContext.tsx
@@ -1,37 +1,58 @@
 import * as React from 'react'
+type State = {
+  size: 'regular' | 'small' | 'minimalist'
+  position: 'tl' | 'tr' | 'bl' | 'br'
+}
 
-type Action = { type: 'set-minimalist'; value: boolean }
+type Action =
+  | { type: 'set-size'; value: State['size'] }
+  | { type: 'set-position'; value: State['position'] }
+  | { type: 'set-settings'; value: State }
 type Dispatch = (action: Action) => void
-type State = { minimalist: boolean }
+
 type SettingsProviderProps = { children: React.ReactNode }
 
 const SettingsStateContext = React.createContext<
   { state: State; dispatch: Dispatch } | undefined
 >(undefined)
 
+const defaultSettings = { size: 'regular', position: 'tl' } as const
+
 function settingsReducer(state: State, action: Action) {
   switch (action.type) {
-    case 'set-minimalist': {
-      return { ...state, minimalist: action.value }
+    case 'set-size': {
+      return { ...state, size: action.value }
     }
-    default: {
-      throw new Error(`Unhandled action type: ${action.type}`)
+    case 'set-position': {
+      return { ...state, position: action.value }
+    }
+    case 'set-settings': {
+      return action.value
     }
   }
 }
 
 function SettingsProvider({ children }: SettingsProviderProps) {
-  const [state, dispatch] = React.useReducer(settingsReducer, {
-    minimalist: false
-  })
+  const [state, dispatch] = React.useReducer(settingsReducer, defaultSettings)
   React.useEffect(() => {
     async function getSettings() {
-      SteamClient.Storage.GetJSON('protondb-badges-settings').then((result) => {
-        dispatch({
-          type: 'set-minimalist',
-          value: JSON.parse(result).minimalist || false
+      SteamClient.Storage.GetJSON('protondb-badges-settings')
+        .then((result) => {
+          const storedSettings = JSON.parse(result)
+          dispatch({
+            type: 'set-settings',
+            value: {
+              size: storedSettings?.size || defaultSettings.size,
+              position: storedSettings.position || defaultSettings.position
+            }
+          })
         })
-      })
+        .catch((e) => {
+          dispatch({
+            type: 'set-settings',
+            value: defaultSettings
+          })
+        })
     }
     getSettings()
   }, [])
