cd972a11f3f5c8f51df35466746cbde0bce2d905
FrogTheFrog <warliukz@gmail.com>
2024-08-09 18:16:53 +0300

    Add missing global Context for settings hook

--- src/index.tsx
+++ src/index.tsx
@@ -5,8 +5,10 @@ import { FaReact } from 'react-icons/fa'
 
 import Settings from './components/settings'
 import patchLibraryApp from './lib/patchLibraryApp'
+import { loadSettings } from './hooks/useSettings'
 
 export default definePlugin(() => {
+  loadSettings()
   const libraryPatch = patchLibraryApp()
   return {
     title: <div className={staticClasses.Title}>ProtonDB Badges</div>,
